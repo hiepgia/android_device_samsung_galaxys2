77e3aee155e4d36ed5b175e19e8071d492cdc4a1
Erol mutlu <cm@erolmutlu.com>
2012-07-20 00:54:50 +0300

    disable OMX libs

--- galaxys2_base.mk
+++ galaxys2_base.mk
@@ -90,23 +90,23 @@ PRODUCT_PACKAGES += \
     libsecmfcapi
 
 # OMX
-PRODUCT_PACKAGES += \
-    libstagefrighthw \
-    libseccscapi \
-    libsecbasecomponent \
+#PRODUCT_PACKAGES += \
+#    libstagefrighthw \
+#    libseccscapi \
+#    libsecbasecomponent \
 #    libsecosal \
-    libSEC_OMX_Resourcemanager \
-    libSEC_OMX_Core \
-    libSEC_OMX_Vdec \
-    libOMX.SEC.AVC.Decoder \
-    libOMX.SEC.M4V.Decoder \
-    libOMX.SEC.WMV.Decoder \
-    libOMX.SEC.VP8.Decoder \
-    libSEC_OMX_Venc \
-    libOMX.SEC.AVC.Encoder \
-    libOMX.SEC.M4V.Encoder \
-    libSEC_OMX_Adec \
-    libOMX.SEC.MP3.Decoder
+#    libSEC_OMX_Resourcemanager \
+#    libSEC_OMX_Core \
+#    libSEC_OMX_Vdec \
+#    libOMX.SEC.AVC.Decoder \
+#    libOMX.SEC.M4V.Decoder \
+#    libOMX.SEC.WMV.Decoder \
+#    libOMX.SEC.VP8.Decoder \
+#    libSEC_OMX_Venc \
+#    libOMX.SEC.AVC.Encoder \
+#    libOMX.SEC.M4V.Encoder \
+#    libSEC_OMX_Adec \
+#    libOMX.SEC.MP3.Decoder
 
 PRODUCT_COPY_FILES += \
 	device/samsung/galaxys2/configs/media_profiles.xml:system/etc/media_profiles.xml
