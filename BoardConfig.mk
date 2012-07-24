9f5d3e6807d79c5a38f3531e897f8ffbc45cdc08
Erol mutlu <cm@erolmutlu.com>
2012-07-24 03:00:18 +0300

    back to cm kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,11 +53,11 @@ BOARD_KERNEL_BASE := 0x40000000
 BOARD_KERNEL_CMDLINE := console=ttySAC2,115200 consoleblank=0
 
 # Inline kernel building
-# TARGET_KERNEL_SOURCE := kernel/samsung/smdk4210
-# TARGET_KERNEL_CONFIG := cyanogenmod_galaxys2_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/smdk4210
+TARGET_KERNEL_CONFIG := cyanogenmod_galaxys2_defconfig
 #Test with siyah kernel ( Teşekkür Ederiz - Gokhan Moral)
-TARGET_KERNEL_SOURCE := gokhanmoral/siyahkernel3
-TARGET_KERNEL_CONFIG := siyah_defconfig
+#TARGET_KERNEL_SOURCE := gokhanmoral/siyahkernel3
+#TARGET_KERNEL_CONFIG := siyah_defconfig
 
 # Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
